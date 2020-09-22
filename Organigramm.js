6bacad86bacd633512a990c8194c03a0cd4c3441
LauraScha <71699938+LauraScha@users.noreply.github.com>
2020-09-22 14:44:55 +0200

    Update Organigramm.js

--- Organigramm.js
+++ Organigramm.js
@@ -84,14 +84,7 @@ return 1;
           
                 }
                 onCustomWidgetBeforeUpdate(changedProperties) {
-                     var myChart = new FusionCharts({
-                    type: "dragnode",
-                    renderAt: "chart-container",
-                    width: "100%",
-                    height: "100%",
-                    dataFormat: "json",
-                    dataSource
-                }).render(); 
+                     
                 }
                     
 });
