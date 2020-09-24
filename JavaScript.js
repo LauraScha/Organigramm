bfe14841cc815c503979f2d849d69a93f620a53b
LauraScha <71699938+LauraScha@users.noreply.github.com>
2020-09-24 14:07:29 +0200

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -1,11 +1,22 @@
+'use strict';
+var http = require('http');
+var port = process.env.PORT || 1337;
+
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+
 (function () {
 
     let template = document.createElement("template");
-    template.innerHTML = `<div id="chart-container">
-            <h3> ORGA </h3></div>  <div>
+    template.innerHTML = `<div>
 <svg width=100%
 height=100%
-id=d3forcebubble></svg></div>`;
+id=d3forcebubble></svg></div>
+<div id="chart-container">
+            <h3> ORGA </h3></div>  `;
 
     class Test extends HTMLElement {
         constructor() {
